e6c58c51181872e58e7da571f9f805ec7af68cdb
MIS803 <MIS803@30803_PC12>
2021-03-16 17:23:12 +0800

    炸彈遊戲

---
+++ 2-2Practice/GenerateBomb.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2_2Practice
+{
+    public partial class GenerateBomb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int[] ia_Bomb = new int[10] { 0, 7, 13, 28, 44, 62, 74, 75, 87, 90 };
+            int i_Row = 0, i_Col = 0;
+            int[,] ia_2DMap = new int[10, 10];
+            
+            for (int i_ct = 0; i_ct < ia_Bomb.Length;)
+        }
+    }
+}
