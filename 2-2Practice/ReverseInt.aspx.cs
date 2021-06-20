ef27446f5ec3ce046023ea6e47ce5bb7eb79e230
康恆 徐 <wa0125678665@>
2021-06-20 08:42:05 +0800

    done

---
+++ 2-2Practice/ReverseInt.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2_2Practice
+{
+    public partial class ReverseInt : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int N = 12345;
+            string n_str = N.ToString();
+            var r = n_str.Reverse().ToArray();
+            n_str = new string(r);
+            int n_int = int.Parse(n_str);
+            Response.Write(n_int);
+        }
+    }
+}
