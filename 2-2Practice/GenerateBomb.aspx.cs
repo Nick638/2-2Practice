877140f3a2eaa5f5c0921e877f73ec3bb405ada3
康恆 徐 <wa0125678665@>
2021-06-20 08:21:07 +0800

    done

--- 2-2Practice/GenerateBomb.aspx.cs
+++ 2-2Practice/GenerateBomb.aspx.cs
@@ -12,10 +12,22 @@ namespace _2_2Practice
         protected void Page_Load(object sender, EventArgs e)
         {
             int[] ia_Bomb = new int[10] { 0, 7, 13, 28, 44, 62, 74, 75, 87, 90 };
-            int i_Row = 0, i_Col = 0;
+            int i_Row = -1;
+            int i_Col = -1;
             int[,] ia_2DMap = new int[10, 10];
 
-            for (int i_ct = 0; i_ct < ia_Bomb.Length;) { }
+            for (int i_Ct = 0; i_Ct < ia_Bomb.Length; i_Ct++)
+            {
+                mt_GetRowCol(ref i_Row, ref i_Col, ia_Bomb[i_Ct]);
+                ia_2DMap[i_Row, i_Col] = -1;
+            }
+
+
+            for (int i_Ct = 0; i_Ct < ia_Bomb.Length; i_Ct++)
+            {
+                mt_AddNumber(ia_Bomb[i_Ct], ref ia_2DMap);
+            }
+            mt_ShowMap(ref ia_2DMap);
         }
     }
 }
